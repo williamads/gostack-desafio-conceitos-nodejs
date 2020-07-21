a6ebfa87eac564599012e5dc189f6e075dc4d7ca
William Silva <williamsilva.uea@gmail.com>
2020-07-20 20:37:33 -0400

    gostack desafio de conceitos NodeJS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,74 @@ app.use(cors());
 
 const repositories = [];
 
+function findIndexByID(id){
+  return repositories.findIndex(repositorie => repositorie.id === id);
+}
+
+function validateRepositorieID(request, response, next){
+  const { id } = request.params;
+
+  if(findIndexByID(id) < 0){
+    return response.status(400).json({ erro: "Repositorie not found!"});
+  }
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateRepositorieID, (request, response) => {
+
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositorieIndex = findIndexByID(id);
+
+  const { likes } = repositories[repositorieIndex];
+
+  const repositorie = { 
+    id, 
+    title, 
+    url, 
+    techs,
+    likes
+  };
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie)
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateRepositorieID, (request, response) => {
+  const { id } = request.params;
+
+  const repositorieIndex = findIndexByID(id);
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateRepositorieID, (request, response) => {
+  const { id } = request.params;
+
+  const repositorieIndex = findIndexByID(id);
+
+  repositories[repositorieIndex].likes++;
+
+  return response.json(repositories[repositorieIndex]);
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const app = require("./app");
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('👌️⚙️ ️ Back-end started!');
+});
